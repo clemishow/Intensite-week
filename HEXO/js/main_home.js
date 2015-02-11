0c7868e8010f43525ae0665a46832bfec989cb70
Saulnier Clement <clement.saulnier@hetic.net>
2015-02-11 15:21:29 +0100

    Change

--- HEXO/js/main_home.js
+++ HEXO/js/main_home.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $('#fullpage').fullpage({
     verticalCentered: true,
     slidesNavigation: true,
-    sectionsColor: ['#F2F2F2', '#F2F2F2', 'white'],
+    sectionsColor: ['#F2F2F2', '#F2F2F2', 'white', '#F2F2F2'],
       afterRender: function(){
         //playing the video
   $('video').get(0).play();
