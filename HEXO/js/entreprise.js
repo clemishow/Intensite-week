e4caec50a2ac39a856838f940c798b685c392deb
Saulnier Clement <clement.saulnier@hetic.net>
2015-02-12 00:56:39 +0100

    change

--- HEXO/js/entreprise.js
+++ HEXO/js/entreprise.js
@@ -4,14 +4,16 @@ $(document).ready(function() {
     anchors: ['firstPage', 'secondPage', 'thirdPage', 'lastPage'],
     verticalCentered: true,
     slidesNavigation: true,
-    sectionsColor: ['#F2F2F2', '#F2F2F2', '#F2F2F2', '#F2F2F2', '#F2F2F2'],
-      afterRender: function(){
-        //playing the video
-  $('video').get(0).play();
-    }
+    sectionsColor: ['#F2F2F2', '#F2F2F2', '#F2F2F2',],
   });
 });
 
 $( "#scroll_down" ).on( "click", function(){
   $.fn.fullpage.moveSectionDown();
+});
+
+
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
 });
