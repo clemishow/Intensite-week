3940e46a8a716256dcf0a413942755d4c050097d
Saulnier Clement <clement.saulnier@hetic.net>
2015-02-11 22:42:05 +0100

    Entreprise

---
+++ HEXO/js/entreprise.js
@@ -0,0 +1,17 @@
+$(document).foundation();
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'lastPage'],
+    verticalCentered: true,
+    slidesNavigation: true,
+    sectionsColor: ['#F2F2F2', '#F2F2F2', '#F2F2F2', '#F2F2F2', '#F2F2F2'],
+      afterRender: function(){
+        //playing the video
+  $('video').get(0).play();
+    }
+  });
+});
+
+$( "#scroll_down" ).on( "click", function(){
+  $.fn.fullpage.moveSectionDown();
+});
