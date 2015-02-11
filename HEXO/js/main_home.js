b270981e089e1c9441f95b2d3fe81e50f0a74574
Saulnier Clement <clement.saulnier@hetic.net>
2015-02-11 16:40:56 +0100

    Chnage

--- HEXO/js/main_home.js
+++ HEXO/js/main_home.js
@@ -1,9 +1,10 @@
 $(document).foundation();
 $(document).ready(function() {
   $('#fullpage').fullpage({
+    anchors: ['home', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixPage'],
     verticalCentered: true,
     slidesNavigation: true,
-    sectionsColor: ['#F2F2F2', '#F2F2F2', 'white', '#F2F2F2'],
+    sectionsColor: ['#F2F2F2', '#F2F2F2', '#F2F2F2', '#F2F2F2'],
       afterRender: function(){
         //playing the video
   $('video').get(0).play();
