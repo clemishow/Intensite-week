2cd31f2d860c16750c429c806377f63dbdf7056f
Saulnier Clement <clement.saulnier@hetic.net>
2015-02-11 18:14:56 +0100

    mobile event/news/socialsN

--- HEXO/js/main_home.js
+++ HEXO/js/main_home.js
@@ -1,10 +1,10 @@
 $(document).foundation();
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    anchors: ['home', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixPage'],
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'lastPage'],
     verticalCentered: true,
     slidesNavigation: true,
-    sectionsColor: ['#F2F2F2', '#F2F2F2', '#F2F2F2', '#F2F2F2'],
+    sectionsColor: ['#F2F2F2', '#F2F2F2', '#F2F2F2', '#F2F2F2', '#F2F2F2'],
       afterRender: function(){
         //playing the video
   $('video').get(0).play();
